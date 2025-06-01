4e83fa469a932381d6b8365c8e17f0628872e1c6
Hades <gbalyukw@gmail.com>
2025-05-31 23:22:52 -0400

    Wizard Logic Update for Sorting and Mapping

--- cdk/lambda/lambda_handler.ts
+++ cdk/lambda/lambda_handler.ts
@@ -4,10 +4,24 @@ export const handler = async () => {
   const url = process.env.MTG_WIZARD_API_URL;
   if (!url) throw new Error('MTG_WIZARD_API_URL Environemnt Variable Missing');
   const response = await axios.get(url);
-  const wizards = response.data.cards;
+  let wizards = response.data.cards;
+
+  wizards.filter(card => !isNaN(parseInt(card.power)))
+  wizards.sort((card1, card2) => parseInt(card1.power) - parseInt(card2.power));
+  wizards = wizards.map(card => ({
+      name: card.name, 
+      type: card.type, 
+      power: card.power, 
+      toughness: card.toughness, 
+      rarity: card.rarity, 
+      flavor: card.flavor, 
+      colors: card.colors, 
+      manaCost: card.manaCost, 
+      imageUrl: card.imageUrl
+    }));
 
   return {
     statusCode: 200,
-    body: JSON.stringify(wizards.slice(0, 10))
+    body: JSON.stringify(wizards)
   };
 };
