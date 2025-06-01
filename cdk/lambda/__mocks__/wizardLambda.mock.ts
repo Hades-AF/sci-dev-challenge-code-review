826b2be1658e003642833c52db3619fe6f5abf96
Hades <gbalyukw@gmail.com>
2025-06-01 03:33:53 -0400

    Fixed code filtering logic bug, and added test with deployment testing.

---
+++ cdk/lambda/__mocks__/wizardLambda.mock.ts
@@ -0,0 +1,38 @@
+export const mockClientWith1ValidWizards = {
+    get: async () => ({
+        data: {
+            cards: [
+                { name: 'Epistolary Librarian', power: '3' }
+            ]
+        }
+    }),
+};
+
+export const mockClientWith3ValidWizards = {
+    get: async () => ({
+        data: {
+            cards: [
+                { name: 'Epistolary Librarian', power: '3' },
+                { name: 'El-Hajjâj', power: '1' },
+                { name: 'Technomancer', power: '5' },
+            ]
+        }
+    }),
+};
+
+export const mockClientWithInvalidWizardPowers = {
+    get: async () => ({
+        data: {
+            cards: [
+                { name: 'Epistolary Librarian', power: '3' },
+                { name: 'El-Hajjâj', power: '!' },
+                { name: 'Technomancer', power: 'X' },
+                { name: 'Master of Etherium', power: '*' },
+            ]
+        }
+    }),
+};
+
+export const mockClientWithInvalidFormat = {
+    get: async () => ({}),
+};
---
+++ cdk/lambda/lambda_handler.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest';
+import { handler } from './lambda_handler';
+import * as wizardMocks from './__mocks__/wizardLambda.mock'
+
+describe('Wizard Lambda', () => {
+    
+    it('returns 200 status code', async () => {
+        process.env.MTG_WIZARD_API_URL = 'https://amazon-mock/wizards';
+
+        const result = await handler(wizardMocks.mockClientWith1ValidWizards);
+
+        expect(result.statusCode).toBe(200);
+    });
+    
+    it('returns 3 wizard cards sorted by power desc', async () => {
+        process.env.MTG_WIZARD_API_URL = 'https://amazon-mock/wizards';
+
+
+        const result = await handler(wizardMocks.mockClientWith3ValidWizards);
+        const body = JSON.parse(result.body);
+
+        expect(body.length).toBe(3);
+        expect(body[0].name).toBe('El-Hajjâj');
+        expect(body[1].name).toBe('Epistolary Librarian');
+        expect(body[2].name).toBe('Technomancer');
+    });
+
+    it('filters out invalid powers', async () => {
+        process.env.MTG_WIZARD_API_URL = 'https://amazon-mock/wizards';
+
+        const result = await handler(wizardMocks.mockClientWithInvalidWizardPowers);
+        const body = JSON.parse(result.body);
+
+        expect(result.statusCode).toBe(200);
+        expect(body.length).toBe(1);
+        expect(body[0].name).toBe('Epistolary Librarian');
+    });
+
+        
+    it('returns 404 status code', async () => {
+        process.env.MTG_WIZARD_API_URL = 'https://amazon-mock/wizards';
+
+
+        const result = await handler(wizardMocks.mockClientWithInvalidFormat);
+        const body = JSON.parse(result.body);
+
+        expect(result.statusCode).toBe(404);
+    });
+});
