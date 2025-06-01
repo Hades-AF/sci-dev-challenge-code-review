1b213ea2a7bfcc9e4b129bd341713e53a2fbd607
Hades <gbalyukw@gmail.com>
2025-06-01 03:33:09 -0400

    Fixed code filtering logic bug, and added test with deployment testing.

--- cdk/lambda/lambda_handler.ts
+++ cdk/lambda/lambda_handler.ts
@@ -1,12 +1,19 @@
 const axios = require('axios');
 
-export const handler = async () => {
+export async function handler(client = axios) {
   const url = process.env.MTG_WIZARD_API_URL;
   if (!url) throw new Error('MTG_WIZARD_API_URL Environemnt Variable Missing');
-  const response = await axios.get(url);
-  let wizards = response.data.cards;
+  const response = await client.get(url);
+  let wizards = response.data?.cards;
 
-  wizards.filter(card => !isNaN(parseInt(card.power)))
+  if (!Array.isArray(wizards) || wizards.length === 0) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: 'No card data obtained.' }),
+    };
+  }
+
+  wizards = wizards.filter(card => !isNaN(parseInt(card.power)))
   wizards.sort((card1, card2) => parseInt(card1.power) - parseInt(card2.power));
   wizards = wizards.map(card => ({
       name: card.name, 
